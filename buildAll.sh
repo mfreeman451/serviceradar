ae54281768692d51a9b9fda9ff2966ba5e12bb28
Michael Freeman <mfreeman451@gmail.com>
2025-01-16 12:16:17 -0600

    helper script for building

---
+++ buildAll.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+./setup-deb-poller.sh
+./setup-deb-dusk-checker.sh
+./setup-deb-agent.sh
+scp release-artifacts/homemon-poller_1.0.0.deb duskadmin@192.168.2.22:~/
+scp release-artifacts/homemon-agent_1.0.0.deb duskadmin@192.168.2.22:~/
+scp release-artifacts/homemon-dusk-checker_1.0.0.deb duskadmin@192.168.2.22:~/

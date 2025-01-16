3d6c19987bf5a93f2e57f615500045c102ac602f
mfreeman451 <mfreeman451@gmail.com>
2025-01-16 15:34:17 -0600

    ♻️ removing unused files

--- buildAll.sh
+++ buildAll.sh
@@ -3,6 +3,8 @@
 ./setup-deb-poller.sh
 ./setup-deb-dusk-checker.sh
 ./setup-deb-agent.sh
+./setup-deb-cloud.sh
+
 scp release-artifacts/homemon-poller_1.0.0.deb duskadmin@192.168.2.22:~/
 scp release-artifacts/homemon-agent_1.0.0.deb duskadmin@192.168.2.22:~/
 scp release-artifacts/homemon-dusk-checker_1.0.0.deb duskadmin@192.168.2.22:~/

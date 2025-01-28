d82101a409fb1eb5ce92c1d2fe4ee4c4b9a10d17
Michael Freeman <mfreeman451@gmail.com>
2025-01-27 22:46:40 -0600

    bump

--- buildAll.sh
+++ buildAll.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=${VERSION:-1.0.7}
+VERSION=${VERSION:-1.0.8}
 
 
 ./setup-deb-poller.sh
--- setup-deb-agent.sh
+++ setup-deb-agent.sh
@@ -2,8 +2,7 @@
 # setup-deb-agent.sh
 set -e  # Exit on any error
 
-# Get version from environment or default to 1.0.7
-VERSION=${VERSION:-1.0.7}
+VERSION=${VERSION:-1.0.8}
 echo "Building serviceradar-agent version ${VERSION}"
 
 echo "Setting up package structure..."

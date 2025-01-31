920e9716dbe7be73e15e44f141e3d599c3b5cc70
Michael Freeman <mfreeman451@gmail.com>
2025-01-31 01:37:14 -0600

    bump

---
+++ buildAll.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+VERSION=${VERSION:-1.0.12}
+
+
+./setup-deb-poller.sh
+./setup-deb-dusk-checker.sh
+./setup-deb-agent.sh
+
+scp release-artifacts/serviceradar-poller_${VERSION}.deb duskadmin@192.168.2.22:~/
+scp release-artifacts/serviceradar-agent_${VERSION}.deb duskadmin@192.168.2.22:~/
+scp release-artifacts/serviceradar-dusk-checker_${VERSION}.deb duskadmin@192.168.2.22:~/

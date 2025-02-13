e2e5854376b59a47918a5b8efb8ebd44d1d6aa07
Michael Freeman <mfreeman451@gmail.com>
2025-02-13 12:29:50 -0600

    updates for 1.0.16

--- buildAll.sh
+++ buildAll.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=${VERSION:-1.0.15}
+VERSION=${VERSION:-1.0.16}
 
 
 ./setup-deb-poller.sh
--- setup-deb-agent.sh
+++ setup-deb-agent.sh
@@ -2,7 +2,7 @@
 # setup-deb-agent.sh
 set -e  # Exit on any error
 
-VERSION=${VERSION:-1.0.15}
+VERSION=${VERSION:-1.0.16}
 echo "Building serviceradar-agent version ${VERSION}"
 
 echo "Setting up package structure..."

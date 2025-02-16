5c5ab02f6f115e021f1f970e60b6aad16d9c7d41
Michael Freeman <mfreeman451@gmail.com>
2025-02-15 20:27:33 -0600

    fixing tests

--- pkg/checker/snmp/collector_test.go
+++ pkg/checker/snmp/collector_test.go
@@ -14,27 +14,41 @@ func TestCollector_WithMocks(t *testing.T) {
 
 	tests := []struct {
 		name      string
-		setupMock func(*MockCollector) error
+		setupMock func(*MockCollector)
+		runTest   func(*MockCollector) error
 		wantErr   bool
 	}{
 		{
 			name: "successful collection",
-			setupMock: func(mc *MockCollector) error {
-				// Set up expectations before calling Start
+			setupMock: func(mc *MockCollector) {
 				dataChan := make(chan DataPoint, 1)
-				mc.EXPECT().GetResults().Return((<-chan DataPoint)(dataChan))
-				mc.EXPECT().Stop().Return(nil)
 				mc.EXPECT().Start(gomock.Any()).Return(nil)
-
-				return nil
+				mc.EXPECT().GetResults().Return((<-chan DataPoint)(dataChan)).AnyTimes()
+				mc.EXPECT().Stop().Return(nil)
+			},
+			runTest: func(mc *MockCollector) error {
+				ctx := context.Background()
+				if err := mc.Start(ctx); err != nil {
+					return err
+				}
+				_ = mc.GetResults() // Ensure GetResults is called
+				return mc.Stop()
 			},
 			wantErr: false,
 		},
 		{
 			name: "start failure",
-			setupMock: func(mc *MockCollector) error {
+			setupMock: func(mc *MockCollector) {
 				mc.EXPECT().Start(gomock.Any()).Return(assert.AnError)
-				return assert.AnError
+				mc.EXPECT().GetResults().Return(make(<-chan DataPoint)).AnyTimes()
+			},
+			runTest: func(mc *MockCollector) error {
+				ctx := context.Background()
+				if err := mc.Start(ctx); err != nil {
+					_ = mc.GetResults() // Ensure GetResults is called even in error case
+					return err
+				}
+				return nil
 			},
 			wantErr: true,
 		},
@@ -43,19 +57,13 @@ func TestCollector_WithMocks(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			mockCollector := NewMockCollector(ctrl)
-			err := tt.setupMock(mockCollector)
+			tt.setupMock(mockCollector)
 
-			ctx := context.Background()
-			actualErr := mockCollector.Start(ctx)
+			err := tt.runTest(mockCollector)
 
 			if tt.wantErr {
-				assert.Error(t, actualErr)
-				assert.Equal(t, err, actualErr)
+				assert.Error(t, err)
 			} else {
-				assert.NoError(t, actualErr)
-
-				// Call Stop for successful tests to satisfy mock expectations
-				err = mockCollector.Stop()
 				assert.NoError(t, err)
 			}
 		})

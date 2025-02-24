d22e222c23883550da81a5f1e215fcf41190f820
Michael Freeman <mfreeman451@gmail.com>
2025-02-23 22:21:48 -0600

    Deploy website - based on 24c08113a4ac01440e828814e2171b8d42ba73e2

--- packaging/agent/scripts/preremove.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-# Only try to manage service if it exists
-if [ -f "/lib/systemd/system/serviceradar-${component_dir}.service" ]; then
-    # Stop and disable service
-    systemctl stop "serviceradar-${component_dir}"
-    systemctl disable "serviceradar-${component_dir}"
-fi

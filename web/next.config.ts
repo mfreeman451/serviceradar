c0f0026c76311cbf59ccad6d492f4c7263b30921
Michael Freeman <mfreeman451@gmail.com>
2025-02-27 00:49:34 -0600

    removing hardcoded backend address

--- web/next.config.ts
+++ web/next.config.ts
@@ -3,8 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     reactStrictMode: true,
     async rewrites() {
-        // const backendUrl = process.env.BACKEND_URL || 'http://localhost:8090';
-        const backendUrl = process.env.BACKEND_URL || 'http://172.233.208.210:8090';
+        const backendUrl = process.env.BACKEND_URL || 'http://localhost:8090';
         return [
             {
                 source: '/api/:path*',

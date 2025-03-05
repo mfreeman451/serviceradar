835af2e3f22ac65dfa9a5c5ec2564258483e3e39
Michael Freeman <mfreeman451@gmail.com>
2025-03-04 21:24:08 -0600

    config fix

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   url: 'https://docs.serviceradar.cloud',
-  baseUrl: '/serviceradar/',
+  baseUrl: '',
 
   organizationName: 'carverauto',
   projectName: 'serviceradar',
